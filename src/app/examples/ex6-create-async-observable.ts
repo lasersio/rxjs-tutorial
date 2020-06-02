0aabe501fb749404818f534792259dd526aed098
Andrey Lukyanenko <andrey.lukyanenko@smartly.io>
2020-06-02 22:34:17 +0300

    examples

--- src/app/examples/ex6-create-async-observable.ts
+++ src/app/examples/ex6-create-async-observable.ts
@@ -1,27 +1,25 @@
 ﻿import { Observable } from 'rxjs';
 
-export function createObservable()
+export function createObservable(n : number)
 {
 	return new Observable(observer => {
-		console.log('Started');
-		observer.next('Reply');
-		observer.complete();
+		for (let i = 0; i < 10; i++) {
+			if (i === n) setTimeout(() => observer.error('Something went wrong'), i);
+
+			setTimeout(() => observer.next(i), i);
+		}
+		setTimeout(() => observer.complete(), 10);
 	});
 }
 
-const obs$ = createObservable();
-
-console.log('Attaching to observables');
-
-obs$.subscribe(
+createObservable(12).subscribe(
 	value => console.log('Observer1 got value: ' + value),
 	error => console.log('Observer1 got error: ' + error),
 	() => console.log('Observer1 completed!\n')
 );
 
-obs$.subscribe(
+createObservable(2).subscribe(
 	value => console.log('Observer2 got value: ' + value),
 	error => console.log('Observer2 got error: ' + error),
 	() => console.log('Observer2 completed!\n')
 );
-
