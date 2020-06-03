edae40983a442958decd7002d8a471974056d809
Andrey Lukyanenko <andrey.lukyanenko@smartly.io>
2020-06-04 01:30:53 +0300

    retrywhen

---
+++ src/examples/exc-delayed-retry.ts
@@ -0,0 +1,21 @@
+﻿import { Observable, of, throwError } from 'rxjs';
+import { delay, mergeMap, retryWhen } from 'rxjs/operators';
+
+const maxRetry = 5;
+
+export function delayedRetry(delayMs : number)
+{
+	let retries = 0;
+
+	return (obs$ : Observable<any>) =>
+		obs$.pipe(
+			retryWhen((errors : Observable<any>) =>
+				errors.pipe(
+					delay(delayMs),
+					mergeMap(error => {
+						return retries++ < maxRetry ? of(error) : throwError(`Failed: Max retry count reached.`);
+					})
+				)
+			)
+		);
+}
